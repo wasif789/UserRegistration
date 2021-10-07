ef48b4fce92d87028f2064907f99d3dde4a50758
wasif789 <wsffarid1@gmail.com>
2021-10-08 00:03:18 +0530

    valid last name

--- UserDetails.cs
+++ UserDetails.cs
@@ -7,10 +7,12 @@ namespace UserRegistration
 {
     class UserDetails
     {
+        public static string lastname;
+        public static string firstname;
+
         public static void GetUserInformation()
         {
             Regex regex = new Regex(@"^[A-Z][a-z]{2,}$");
-            string firstname;
             Console.WriteLine("\nEnter First Name starting with Caps(min 3 characters)");
             while (true)
             {
@@ -24,7 +26,23 @@ namespace UserRegistration
                     Console.WriteLine("Please enter a Valid First Name!");
                 }
             }
+
+            //Get Last Name from user
+            Console.WriteLine("\nEnter Last Name starting with Caps(minimum 3 characters)");
+            while (true)
+            {
+                lastname = Console.ReadLine();
+                if (regex.IsMatch(lastname))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Name Name!");
+                }
+            }
             Console.WriteLine("Your First name is: {0}", firstname);
+            Console.WriteLine("Your Last name is: {0}", lastname);
         }
     }
 }
