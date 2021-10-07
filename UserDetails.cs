c45ef2b862dce2a0945b13636d1d287c813a64b4
wasif789 <wsffarid1@gmail.com>
2021-10-08 00:12:39 +0530

    valid email

--- UserDetails.cs
+++ UserDetails.cs
@@ -9,10 +9,12 @@ namespace UserRegistration
     {
         public static string lastname;
         public static string firstname;
+        public static string emails;
 
         public static void GetUserInformation()
         {
             Regex regex = new Regex(@"^[A-Z][a-z]{2,}$");
+            Regex email = new Regex(@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
             Console.WriteLine("\nEnter First Name starting with Caps(min 3 characters)");
             while (true)
             {
@@ -41,8 +43,23 @@ namespace UserRegistration
                     Console.WriteLine("Please enter a Valid Name Name!");
                 }
             }
+            //Get email from user
+            Console.WriteLine("\nEnter Email Address");
+            while (true)
+            {
+                emails = Console.ReadLine();
+                if (email.IsMatch(emails))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a Valid Email!");
+                }
+            }
             Console.WriteLine("Your First name is: {0}", firstname);
             Console.WriteLine("Your Last name is: {0}", lastname);
+            Console.WriteLine("Your Email is: {0}", emails);
         }
     }
 }
